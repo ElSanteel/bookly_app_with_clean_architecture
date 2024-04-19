c8f69684a0746e5f7c00d1e2dc6b9fd36d7fb219
ElSanteel <alsantel.ma@gmail.com>
2024-04-19 23:33:10 +0200

    make Either class and failure.dart file

--- lib/Features/Home/domain/repos/home_repo.dart
+++ lib/Features/Home/domain/repos/home_repo.dart
@@ -1,7 +1,10 @@
 import 'package:book_store_app/Features/Home/domain/entities/book_entity.dart';
+import 'package:book_store_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 // بيحدد ايه الهيحصل في الفيتشر دي
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
+  // Either Class from dartz package make me return two types instead of one type
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
   Future<List<BookEntity>> fetchNewestBooks();
 }
