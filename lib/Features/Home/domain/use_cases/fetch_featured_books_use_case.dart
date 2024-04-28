f86884f2524ead74707e126d6b870219443ec6f8
ElSanteel <alsantel.ma@gmail.com>
2024-04-28 17:00:24 +0300

    refactor to use case class

--- lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -2,13 +2,19 @@ import 'package:book_store_app/Features/Home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
 import '../entities/book_entity.dart';
 
-class FetchFeaturedBookUseCase {
+class FetchFeaturedBookUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBookUseCase(this.homeRepo);
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    // TODO: implement call
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
+// class denotes that i will not send anything
